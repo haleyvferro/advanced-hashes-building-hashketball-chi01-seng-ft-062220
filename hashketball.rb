6ad585abc39fcbfbc947edebd907eacaad6824ee
Haley Ferro <haleyvferro@gmail.com>
2020-06-18 03:01:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,7 @@
 game_hash = {
   :home = {
     :team_name = "Brooklyn Nets",
-    :colors = ["Black", "White"]
+    :colors = ["Black", "White"],
     :players = [ {
       :player_name = "Alan Anderson"
       :number = 0, 
@@ -11,18 +11,18 @@ game_hash = {
       :rebounds = 12, 
       :assists = 12,
       :steals = 3,
-      :blocks = 1 
+      :blocks = 1,
       :slam_dunks = 1 
-      }
+      },
       {
-      :player_name = "Reggie Evans"
-      :number =  30
-      :shoe = 14 
-      :points = 12
-      :rebounds = 12
-      :assists = 12
-      :steals = 12
-      :blocks = 12
+      :player_name = "Reggie Evans",
+      :number =  30,
+      :shoe = 14,
+      :points = 12,
+      :rebounds = 12,
+      :assists = 12,
+      :steals = 12,
+      :blocks = 12,
       :slam_dunks = 7
       }
       {

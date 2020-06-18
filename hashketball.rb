523f3878bcdaf7edf1834044b22081cdffe3d65a
Haley Ferro <haleyvferro@gmail.com>
2020-06-18 02:52:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,12 +1,78 @@
 # Write your code here!
 game_hash = {
   :home = {
+    :team_name = "Brooklyn Nets"
+    :colors = ["Black", "White"]
+    :players = [ {
+      :player_name = "Alan Anderson"
+      :number = 0 
+      :shoe = 16 
+      :points = 22 
+      :rebounds = 12 
+      :assists = 12
+      :steals = 
+      :blocks = 
+      :slam_dunks =
+      }
+      {
+      :player_name = 
+      :number = 
+      :shoe = 
+      :points = 
+      :rebounds =
+      :assists = 
+      :steals = 
+      :blocks = 
+      :slam_dunks =
+      }
+      {
+      :player_name = 
+      :number = 
+      :shoe = 
+      :points = 
+      :rebounds =
+      :assists = 
+      :steals = 
+      :blocks = 
+      :slam_dunks =
+      }
+      {
+      :player_name = 
+      :number = 
+      :shoe = 
+      :points = 
+      :rebounds =
+      :assists = 
+      :steals = 
+      :blocks = 
+      :slam_dunks =
+      }
+      {
+      :player_name = 
+      :number = 
+      :shoe = 
+      :points = 
+      :rebounds =
+      :assists = 
+      :steals = 
+      :blocks = 
+      :slam_dunks =
+      }]
+  }
+  
+  :away = {
     :team_name = ""
     :colors = []
-    :players = [
-      :play]
-  }
-  :away = {
-    
+    :players = [ {
+      :player_name = 
+      :number = 
+      :shoe = 
+      :points = 
+      :rebounds =
+      :assists = 
+      :steals = 
+      :blocks = 
+      :slam_dunks =
+      } ]
   }
 }
